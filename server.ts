abb8699c32c3114b7542cc330514e6a180222aa9
lerichardson <litbelb1@outlook.com>
2021-07-17 19:37:45 -0700

    📦 Add CORS because why not

--- server.ts
+++ server.ts
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+// Server settings
+app.use(cors());
+app.use(express.static('public'));
 
 app.get('/', function (req: any, res: any) {
     res.send('Hello World!');
