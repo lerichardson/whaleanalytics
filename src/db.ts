b737d9965b8305135122d17acf12aa22ba4d2f3e
lerichardson <litbelb1@outlook.com>
2021-07-27 14:51:34 -0700

    🐛 oops, wrong process.env name hehe

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sql = new Sequelize(`postgres://postgres:${process.env.DB_PASS}@db.fyipxrvzaupgdtfnwlyy.supabase.co:5432/postgres`);
+const sql = new Sequelize(`postgres://postgres:${process.env.DB_PASSWORD}@db.fyipxrvzaupgdtfnwlyy.supabase.co:5432/postgres`);
 
 const connection = sql
 
